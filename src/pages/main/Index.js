8efd2f89cfcbf6654dbf79c542ff31a4545b60d0
ggc <15555124010@163.com>
2019-07-24 10:31:27 +0800

    0.0

--- src/pages/main/Index.js
+++ src/pages/main/Index.js
@@ -1,11 +1,12 @@
 import React from "react"
+import styles from "./index.css"
 
 class Index extends React.Component {
     render() {
         return (
-            <h1>你好</h1>
+            <h1 className={styles.red} title="123">你好</h1>
         );
     }
 }
 
-export  default Index
+export default Index
